4edac24d39a6a0f1456195334c30701e0ffb46fd
leonyonz <leonachild@gmail.com>
2020-12-17 01:42:41 +0700

    upload source

---
+++ _drafts/2020-12-17-Docker-Nginx.md
@@ -0,0 +1,11 @@
+---
+title: Writing a New Post
+lang: id_ID
+layout: post
+date: 2020-12-17 01:40:11 +0700
+author: Leon Sastra
+categories:
+    - nama-kategori
+tags:
+    - nama-tag
+---
---
+++ inisiate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#export path=$PWD/tools
+
+# Remove previous binary
+
+rm -fv /usr/bin/push
+rm -fv /usr/bin/new-post
+
+# Get your path git
+echo "Saat ini berada pada directory $PWD"
+echo ""
+read -p "Masukan Path Git (ex: /xxx/xxx/belajar-linux-id) : " path
+
+# Check tools dir is exist
+mkdir -v $path/tools
+
+# Download Default File
+echo "Start downloading default tools ====>"
+wget "https://gitlab.com/leonyonz/mediumish-theme-jekyll/-/snippets/2051126/raw/master/push.sh" -O $path/tools/push.sh
+wget "https://gitlab.com/leonyonz/mediumish-theme-jekyll/-/snippets/2051127/raw/master/new-post.sh" -O $path/tools/new-post.sh
+
+# Define variable in tools
+sed -i "s|path|$path|g" $path/tools/new-post.sh
+sed -i "s|path|$path|g" $path/tools/push.sh
+
+# Change permission
+echo "Change permission tools ====>"
+chmod +x $path/tools/*
+
+ls -la $path/tools/
+
+# Make binary for tools
+echo ""
+echo "Send tool to binary path ====>"
+mv $path/tools/new-post.sh /usr/bin/new-post
+mv $path/tools/push.sh /usr/bin/push
+
+#Check
+echo "Tes command baru ===>"
+which new-post
+which push
+echo ""
+echo "Inisiate Done, Please refer to : https://gitlab.com/hiblinux/belajar-linux-id"
